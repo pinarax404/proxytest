8d3aad21d344941230a8cba48c89e929c43728d6
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-12 12:16:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ function pinarax_start($ptype, $proxy) {
 
 function pinarax_curl_attr($ptype, $proxy, $url) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://ipwhois.app/json/');
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
